6c96842ac1d323c52c36ba2a26af3a55bdd03781
riwuko x <riwuko@gmail.com>
2020-07-06 10:40:25 +0200

    added new cv template

--- src/components/CvPage.js
+++ src/components/CvPage.js
@@ -11,120 +11,130 @@ export default class CvPage extends Component{
         const phone = "Phone: "+data["contactInfo"]["phone"];
         const github = "Github: "+data["contactInfo"]["github"];
         return(
-        <table>
-        <tr><EditableText value={email}/></tr>
-        <tr><EditableText value={phone}/></tr>
-        <tr><EditableText value={github}/></tr>
-        </table>
+        <div className='personal-data-item'>
+            <h3><EditableText value="Contact details"/></h3>
+            <table>
+            <tr><EditableText value={email}/></tr>
+            <tr><EditableText value={phone}/></tr>
+            <tr><EditableText value={github}/></tr>
+            </table>
+        </div>
         );
     }
 
     generatePersonalInfo(){
         const description = data["personalInfo"]["text"];
         return(
+            <div className='personal-data-item'>
+            <h3><EditableText value="Personal background"/></h3>
             <EditableText value={description}/>
+            </div>
         );
     }
 
     generateAcademicInfo(){
         return(
-            <table>
-            <tr>
-                <td className="dates-range"><EditableText value={data["academicInfo"]["1"]["dates"]}/></td>
-                <td><EditableText value={data["academicInfo"]["1"]["school"]}/></td>
-            </tr>
-            <tr>
-                <td className="dates-range"><EditableText value={data["academicInfo"]["0"]["dates"]}/></td>
-                <td><EditableText value={data["academicInfo"]["0"]["school"]}/></td>
-            </tr>
-        </table>
+            <div className='personal-data-item'>
+                <h3><EditableText value="Academic profile"/></h3>
+                <table>
+                <tr>
+                    <td className="dates-range"><EditableText value={data["academicInfo"]["1"]["dates"]}/></td>
+                    <td><EditableText value={data["academicInfo"]["1"]["school"]}/></td>
+                </tr>
+                <tr>
+                    <td className="dates-range"><EditableText value={data["academicInfo"]["0"]["dates"]}/></td>
+                    <td><EditableText value={data["academicInfo"]["0"]["school"]}/></td>
+                </tr>
+            </table>
+        </div>
         );
     }
 
     generateWorkInfo(){
         return(
-            <table>
-                <tr>
-                    <td className="dates-range"><EditableText value={data["workInfo"]["0"]["dates"]}/></td>
-                    <td><EditableText value={data["workInfo"]["0"]["work"]}/></td>
-                </tr>
-                <tr>
-                    <td className="dates-range"><EditableText value={data["workInfo"]["1"]["dates"]}/></td>
-                    <td><EditableText value={data["workInfo"]["1"]["work"]}/></td>
-                </tr>
-            </table>
+            <div className='personal-data-item'>
+                <h3><EditableText value="Work experience"/></h3>
+                <table>
+                    <tr>
+                        <td className="dates-range"><EditableText value={data["workInfo"]["0"]["dates"]}/></td>
+                        <td><EditableText value={data["workInfo"]["0"]["work"]}/></td>
+                    </tr>
+                    <tr>
+                        <td className="dates-range"><EditableText value={data["workInfo"]["1"]["dates"]}/></td>
+                        <td><EditableText value={data["workInfo"]["1"]["work"]}/></td>
+                    </tr>
+                </table>
+            </div>
         );
     }
 
     generateProjectsInfo(){
         return(
-            <table>
-                <div className="single-project">
-                <tr className="project-title">
-                <EditableText value={data["projects"]["0"]["title"]} />
-                </tr>
-                <tr className="project-description">
-                <EditableText value={data["projects"]["0"]["description"]} />
-                </tr>
-                <tr className="project-technologies">
-                <EditableText value={data["projects"]["0"]["technologies"]} />
-                </tr>
-                </div>
-                <div className="single-project">
-                <tr className="project-title">
-                <EditableText value={data["projects"]["1"]["title"]} />
-                </tr>
-                <tr className="project-description">
-                <EditableText value={data["projects"]["1"]["description"]} />
-                </tr>
-                <tr className="project-technologies">
-                <EditableText value={data["projects"]["1"]["technologies"]} />
-                </tr>
-                </div>
-                <div className="single-project">
-                <tr className="project-title">
-                <EditableText value={data["projects"]["2"]["title"]} />
-                </tr>
-                <tr className="project-description">
-                <EditableText value={data["projects"]["2"]["description"]} />
-                </tr>
-                <tr className="project-technologies">
-                <EditableText value={data["projects"]["2"]["technologies"]} />
-                </tr>
-                </div>
-            </table>
+            <div className='personal-data-item-external'>
+                <h3><EditableText value="Important projects"/></h3>
+                <table>
+                    <div className="single-project">
+                    <tr className="project-title">
+                    <EditableText value={data["projects"]["0"]["title"]} />
+                    </tr>
+                    <tr className="project-description">
+                    <EditableText value={data["projects"]["0"]["description"]} />
+                    </tr>
+                    <tr className="project-technologies">
+                    <EditableText value={data["projects"]["0"]["technologies"]} />
+                    </tr>
+                    </div>
+                    <div className="single-project">
+                    <tr className="project-title">
+                    <EditableText value={data["projects"]["1"]["title"]} />
+                    </tr>
+                    <tr className="project-description">
+                    <EditableText value={data["projects"]["1"]["description"]} />
+                    </tr>
+                    <tr className="project-technologies">
+                    <EditableText value={data["projects"]["1"]["technologies"]} />
+                    </tr>
+                    </div>
+                    <div className="single-project">
+                    <tr className="project-title">
+                    <EditableText value={data["projects"]["2"]["title"]} />
+                    </tr>
+                    <tr className="project-description">
+                    <EditableText value={data["projects"]["2"]["description"]} />
+                    </tr>
+                    <tr className="project-technologies">
+                    <EditableText value={data["projects"]["2"]["technologies"]} />
+                    </tr>
+                    </div>
+                </table>
+            </div>
         );
     }
 
     generateScheme1(){
         return(
             <div >
-            <section className='table'>
-            <div className='table-left-col'>
-                <div className='personal-data-item'>
-                    <h3><EditableText value="Work experience"/></h3>
-                        {this.generateWorkInfo()}
+                <section className='table'>
+                    <div className='table-left-col'>
+                        <div>
+                            {this.generateWorkInfo()}
+                        </div>
+                        <div>
+                            {this.generateAcademicInfo()}
+                        </div>
+                    </div>
+                    <div className='table-right-col'>
+                        <div>
+                            {this.generatePersonalInfo()}
+                        </div>
+                        <div>
+                            {this.generateContactInfo()}
+                        </div>
+                    </div>
+                </section>
+                <div>
+                    {this.generateProjectsInfo()}
                 </div>
-                <div className='personal-data-item'>
-                    <h3><EditableText value="Academic profile"/></h3>
-                        {this.generateAcademicInfo()}
-                </div>
-            </div>
-            <div className='table-right-col'>
-                <div className='personal-data-item'>
-                    <h3><EditableText value="Personal background"/></h3>
-                        {this.generatePersonalInfo()}
-                </div>
-                <div className='personal-data-item'>
-                    <h3><EditableText value="Contact details"/></h3>
-                        {this.generateContactInfo()}
-                </div>
-            </div>
-            </section>
-            <div className='personal-data-item-external'>
-                <h3><EditableText value="Important projects"/></h3>
-                        {this.generateProjectsInfo()}
-            </div>
             </div>
         );
     }
@@ -134,23 +144,22 @@ export default class CvPage extends Component{
             <div>
                 <section className='table'>
                     <div className='table-left-col'>
-                        <div className='personal-data-item'>
-                            <h3><EditableText value="Personal detail"/></h3>
+                        <div>
                             {this.generatePersonalInfo()}
                         </div>
-                        <div className='personal-data-item'>
-                            <h3><EditableText value="Contact details"/></h3>
+                        <div>
                             {this.generateContactInfo()}
                         </div>
                     </div>
                     <div className="table-right-col">
-                        <div className='personal-data-item'>
-                            <h3><EditableText value="Work experience"/></h3>
+                        <div>
                             {this.generateWorkInfo()}
                         </div>
-                        <div className='personal-data-item'>
-                            <h3><EditableText value="Academic profile"/></h3>
+                        <div>
                             {this.generateAcademicInfo()}
+                        </div>
+                        <div>
+                            {this.generateProjectsInfo()}
                         </div>
                     </div>
                 </section>
